99643c78787961f6db811f501798c4312ee319ca
Narong <sovannarong.raing@gmail.com>
2021-03-03 17:15:18 +0700

    update

--- www/wp-content/themes/luna/platform/desk/views/page/dv-page-test.php
+++ www/wp-content/themes/luna/platform/desk/views/page/dv-page-test.php
@@ -1,6 +1,6 @@
 <div class="content home">
 
-   <div class="section most-popular">
+   <!-- <div class="section most-popular">
       <div class="container-fluid ">
          <div class="container">
             <div class="title">
@@ -10,7 +10,7 @@
          </div>
 
          <div class="drink-list">
-            <?php for($i=0; $i<2; $i++){ ?>
+            <?php for($i=0; $i<5; $i++){ ?>
                <div class="drink">
                   <div class="overlay">
                      <h5>Pandan Cream Puff</h5>
@@ -19,6 +19,30 @@
             <?php } ?>
          </div>
         
+      </div>
+   </div> -->
+
+   <div class="mobie-app">
+
+      <div class="thumbnail">
+         <div class="background">
+            background
+         </div>
+      </div>
+
+      <div class="right">
+         <div class="description">
+            <h4>Download our app for free!</h4>
+            <p>Do you love to order coffee and bakery online from our coffee shop? 
+               Use this app to personalize your experience and benefit of the easiest, 
+               fastest way to order your favorite coffee and bakery.</p>
+            <div class="link">
+               <a class="qrcode">d</a>
+               <a class="t">d</a>
+               <a class="icon android">d</a>
+               <a class="icon ios">d</a>
+            </div>
+         </div>
       </div>
    </div>
 
