cbcf8e5bb03632ca350c7c67a16a0d2e579e7264
Narong <sovannarong.raing@gmail.com>
2021-03-11 17:05:46 +0700

    update

--- www/wp-content/themes/luna/platform/desk/desk-footer.php
+++ www/wp-content/themes/luna/platform/desk/desk-footer.php
@@ -9,14 +9,24 @@
                 <div class="container">
                     <div class="overview">
                         <a><img src="<?php bloginfo('template_url');?>/toolsets/images/lunacoffee&bakery.png"  alt="" /></a>
+                        <p> Morbi varius suscipit augue non cursus.Aenean malesuada laoreet magna finibus
+                            accumsan.Proin volutpat mi lacus,a interdum tortor tempus sed. Intger condimentum
+                            finibus justo,nec varius velit luctus ut.Nunc congue massa in turpis euismod
+                            commodo.Mauris nec ligulaposuere.
+                        </p>
                     </div>
                     <div class="quicklink">
-                        <p></p>
+                        d<!-- <?php wp_nav_menu( array( 'theme_location' => 'quickLinkMenu' ) ); ?> -->
                     </div>
-                    <div class="copyright">
-
+                    <div class="copyright"> 
+                        <p>Copyright © 2021 Luna Coffee @ Bakery, All Rights Reserved.</p>
+                        <span>Contactless payment we accept.</span>        
+                        <ul>
+                            <li>d</li>
+                            <li>d</li>
+                            <li>d</li>
+                        </ul>            
                     </div>
-
                 </div>
             </div>
         </div>
